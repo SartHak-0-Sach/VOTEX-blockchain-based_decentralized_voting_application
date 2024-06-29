2e391695a6ee28cdf68454571ecaac1fa1ddef67
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-29 15:57:53 +0530

    Create TextContainer.jsx

---
+++ src/TextContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TextContainer.css'; 
+
+const TextContainer = () =>
+{
+  return (
+    <div className="text-container">
+      <div className="heading">Heading</div>
+      <div className="body-text">
+        This is the body text of the text container.
+      </div>
+    </div>
+  );
+}
+
+export default TextContainer;
